1b6bb6c17d4768f40e532a77181b4716c42c7a85
RicardoJeske <ricardor.jeske@hotmail.com>
2014-10-21 13:38:14 -0300

    Signed-off-by: RicardoJeske <ricardor.jeske@hotmail.com>

---
+++ layout/js/medidas.js
@@ -0,0 +1,12 @@
+
+var spanLargura = document.getElementById('largura');
+var spanAltura = document.getElementById('altura');
+
+window.onresize = function (){
+    altura = window.innerHeight;
+    largura = window.innerWidth;
+    
+    spanLargura.innerHTML = largura + 'px ';
+    spanAltura.innerHTML = altura + 'px ';
+};
+
